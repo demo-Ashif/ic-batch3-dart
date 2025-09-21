8996faf405433abb7f1f9ed6f3fa04b5ed8e4537
Ashif Mujtoba <nmujtoba@gmail.com>
2025-09-22 03:23:38 +0600

    class 12 code added

--- bin/ic_batch3_dart.dart
+++ bin/ic_batch3_dart.dart
@@ -1,5 +1,1 @@
-import 'package:ic_batch3_dart/ic_batch3_dart.dart' as ic_batch3_dart;
-
-void main(List<String> arguments) {
-  print('Hello world: ${ic_batch3_dart.calculate()}!');
-}
+void main(List<String> arguments) {}
